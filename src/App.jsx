c9330316ff6e73a6c7ef9563c53d98317a131913
Danny Le <le.dannyj@gmail.com>
2021-06-04 12:38:47 -0400

    Set up all clear and number click function

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export function App() {
+  const [display, setDisplay] = useState(0)
+
+  const [firstNum, setFirstNum] = useState(0)
+  const [secondNum, setSecondNum] = useState(0)
+
+  const [total, setTotal] = useState(0)
+
+  function handleNumClick(event) {
+    setDisplay(event.target.value)
+  }
+
+  function handleAllClear() {
+    setDisplay(0)
+    setFirstNum(0)
+    setSecondNum(0)
+    setTotal(0)
+  }
+
   return (
     <main>
       <div className="calculator">
-        <div className="display">0</div>
+        <div className="display">{display}</div>
         <div className="buttons">
-          <button className="button fn">AC</button>
+          <button className="button fn" onClick={handleAllClear}>
+            AC
+          </button>
           <button className="button fn">&plusmn;</button>
           <button className="button fn">%</button>
           <button className="button op">&divide;</button>
-          <button className="button">7</button>
-          <button className="button">8</button>
-          <button className="button">9</button>
-          <button className="button op">&times;</button>
-          <button className="button">4</button>
-          <button className="button">5</button>
-          <button className="button">6</button>
-          <button className="button op">&minus;</button>
-          <button className="button">1</button>
-          <button className="button">2</button>
-          <button className="button">3</button>
+          <button className="button" value={7} onClick={handleNumClick}>
+            7
+          </button>
+          <button className="button" value={8} onClick={handleNumClick}>
+            8
+          </button>
+          <button className="button" value={9} onClick={handleNumClick}>
+            9
+          </button>
+          <button className="button op">*</button>
+          <button className="button" value={4} onClick={handleNumClick}>
+            4
+          </button>
+          <button className="button" value={5} onClick={handleNumClick}>
+            5
+          </button>
+          <button className="button" value={6} onClick={handleNumClick}>
+            6
+          </button>
+          <button className="button op">-</button>
+          <button className="button" value={1} onClick={handleNumClick}>
+            1
+          </button>
+          <button className="button" value={2} onClick={handleNumClick}>
+            2
+          </button>
+          <button className="button" value={3} onClick={handleNumClick}>
+            3
+          </button>
           <button className="button op">+</button>
-          <button className="button x2">0</button>
+          <button className="button x2" value={0} onClick={handleNumClick}>
+            0
+          </button>
           <button className="button">.</button>
           <button className="button op">=</button>
         </div>
